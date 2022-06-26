9321c78f638cba13dc7a30154fd3a9f3ffb2112d
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-26 20:52:35 +0200

    Create style.py

---
+++ style_display/style.py
@@ -0,0 +1,25 @@
+card_icon = {
+    "color": "green",
+    "textAlign": "center",
+    "fontSize": "4em",
+    "margin": "auto",
+}
+
+equipment_widget_style = {"backgroundColor": "#3CB371", "maxWidth": 75}
+
+button_style = {
+    "width": "110%",
+    "borderRadius": 0,
+    "backgroundColor": "#696969",
+}
+
+
+sitename_dropdown_style = {"paddingLeft": "50%", "backgroundColor": "#696969"}
+
+page_style = {"backgroundColor": "#154C79"}
+homepage_icon_style = {
+                        "opacity": 0.3,
+                        "objectFit": "cover",
+                        "height": "100%",
+                        "width": "100%",
+                        }
