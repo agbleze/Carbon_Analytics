455ac5427cc92f4718fa0baee1671f0290161dd7
Agbleze <lagbleze@st.ug.edu.gh>
2022-06-27 14:17:10 +0200

    Create style.py

---
+++ StyleDisplay/style.py
@@ -0,0 +1,53 @@
+card_icon = {
+    "color": "green",
+    "textAlign": "center",
+    "fontSize": "4em",
+    "margin": "auto",
+}
+
+equipment_widget_style = {"backgroundColor": "#3CB371", "maxWidth": 75}
+
+button_style = {
+    "width": "110%",
+    "borderRadius": 0,
+    "backgroundColor": "#696969",
+}
+
+
+sitename_dropdown_style = {"paddingLeft": "50%", "backgroundColor": "#696969"}
+
+page_style = {"backgroundColor": "#154C79"}
+homepage_icon_style = {
+                        "opacity": 0.3,
+                        "objectFit": "cover",
+                        "height": "100%",
+                        "width": "100%",
+                        }
+
+cardimg_style = {'width': '100%', 'height': '100%', 
+              "opacity": 0.2, "objectFit": "cover"
+              }
+cardbody_style = {"margin": "2%"}
+
+card_style = {"width": "20rem", "height": "20rem"}
+card_icon = {
+    "color": "blue",
+    "textAlign": "center",
+    "fontSize": "4em",
+    "margin": "auto"
+}
+
+page_style = {"backgroundColor": "#154C79"}
+
+style={"maxWidth": 195, 
+       "backgroundColor": "#e4ac23",
+},
+
+outputcard_style={
+        "maxWidth": 195,
+        "backgroundColor": "#e4ac23",
+    }
+
+popover_head_style = {"backgroundColor": " #1AFF1A"}
+
+
