ff9e31bd000d5a3ee2972afef1d865f8c967f909
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-14 23:56:06 +0200

    Update Co2LassoRegressor.py

--- models/Co2LassoRegressor.py
+++ models/Co2LassoRegressor.py
@@ -1,8 +1,10 @@
 # make lasso model
 from sklearn.linear_model import LassoCV
 from sklearn.pipeline import make_pipeline
+from sklearn.metrics import mean_squared_error
 from models.preprocess_pipeline import linear_model_preprocess_pipeline
 import numpy as np
+
 
 
 lasso_pipeline = make_pipeline(linear_model_preprocess_pipeline,
@@ -19,3 +21,14 @@ if __name__ == '__main__':
                         'tol': np.arange(0.0001, 1, 0.001),
                         'n_alpha': range(100, 1000, 100)
                     }
+
+    #%% lasso prediction
+    lasso_pipeline.fit(X_raw_train, y_raw_train)
+
+    #%%
+    y_pred_lasso = lasso_pipeline.predict(X_raw_test)
+
+    mean_squared_error(y_true=y_raw_test, y_pred=y_pred_lasso, squared=False)
+
+
+
