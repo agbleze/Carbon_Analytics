3e19b0f870b237f50f016ccd84ab0239cd2d1e68
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-17 23:07:24 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,2 +1,20 @@
-from dash import Input, Output, State
+from dash import Input, Output, State, callback
 from dash.exceptions import PreventUpdate
+
+
+
+@callback(
+    Output(component_id="main_content", component_property="children"),
+    Input(component_id="location", component_property="href"),
+)
+def show_page_display(href):
+    site_page = href
+    site_to_view = site_page.split("/")[-1]
+    if site_to_view == "carbon_analytics":
+        return analytics_layout 
+    elif site_to_view == 'emission_prediction':
+        return carbon_emission
+    elif site_to_view == 'hypothesis':
+        return hypothesis_layout
+    else:
+        return app_description
