c222ca01eccdafc2a12c3307cb5bc576250b6f8f
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-22 03:44:22 +0200

    Update Co2Rfregressor.py

--- models/Co2Rfregressor.py
+++ models/Co2Rfregressor.py
@@ -1,7 +1,12 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import make_pipeline
-from sklearn.metrics import mean_squared_error
-from models.preprocess_pipeline import decision_tree_data_preprocess
+from sklearn.metrics import mean_squared_error, r2_score
+from preprocess_pipeline import (decision_tree_data_preprocess,
+                                 X_train, 
+                                 X_test, 
+                                 y_train,
+                                 y_test
+                                )
 
 rf = RandomForestRegressor(random_state=0)
 rf_pipeline = make_pipeline(decision_tree_data_preprocess, rf)
@@ -10,11 +15,12 @@ rf_pipeline = make_pipeline(decision_tree_data_preprocess, rf)
 
 if __name__ == '__main__':
         #%% rf prediction
-    rf_pipeline.fit(X_raw_train, y_raw_train)
+    rf_pipeline.fit(X_train, y_train)
 
     #%%
-    y_pred_rf = rf_pipeline.predict(X_raw_test)
-    mean_squared_error(y_true=y_raw_test, y_pred=y_pred_rf, squared=False)
+    y_pred_rf = rf_pipeline.predict(X_test)
+    rf_rmse_test = mean_squared_error(y_true=y_test, y_pred=y_pred_rf, squared=False)
+    print(f'Random forest test RMSE: {rf_rmse_test}')
 
 
 
