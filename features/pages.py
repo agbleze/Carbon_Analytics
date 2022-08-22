0c013bb4448825c37fe7d689cdcd99569c85e947
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-22 22:29:25 +0200

    Update pages.py

--- features/pages.py
+++ features/pages.py
@@ -1,16 +1,17 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-from style_display.style import  homepage_icon_style, page_style
+from StyleDisplay.style import  homepage_icon_style, page_style
 
 
 
-def create_page_with_card_button(self,title: str = 'Climate Analytics',
+def create_page_with_card_button(#self,
+                                 title: str = 'Climate Analytics',
                                  card1_title:str = 'Carbon Emission Estimate',
                                  card2_title: str = 'Carbon Emission Prediction',
                                  card3_title: str = 'Statistical Analysis and \
                                                      Hypothesis Testing'):
  
-    app_description = html.Div(
+    return dbc.Container(
         style=page_style,
         children=[
             dbc.Row(
@@ -23,14 +24,6 @@ def create_page_with_card_button(self,title: str = 'Climate Analytics',
                             ),
                             html.Br(),
                             html.Br(),
-                            # dbc.Modal(
-                            #     id="nodata_popup",
-                            #     is_open=False,
-                            #     children=[
-                            #         dbc.ModalHeader(dbc.ModalTitle(id="site_name")),
-                            #         dbc.ModalBody(id="desc_site_info"),
-                            #     ],
-                            # ),
                         ]
                     )
                 ]
@@ -59,7 +52,7 @@ def create_page_with_card_button(self,title: str = 'Climate Analytics',
                                                 ]
                                             )
                                         ],
-                                        href="asset",
+                                        href="carbon_analytics",
                                     ),
                                 ],
                                 style={"width": "18rem", "height": "18rem"},
@@ -86,7 +79,7 @@ def create_page_with_card_button(self,title: str = 'Climate Analytics',
                                                 ]
                                             )
                                         ],
-                                        href="site_asset",
+                                        href="emission_prediction",
                                     ),
                                 ],
                                 style={"width": "18rem", "height": "18rem"},
@@ -113,7 +106,7 @@ def create_page_with_card_button(self,title: str = 'Climate Analytics',
                                                 ]
                                             )
                                         ],
-                                        href="site_weather",
+                                        href="hypothesis",
                                     ),
                                 ],
                                 style={"width": "18rem", "height": "18rem"},
@@ -123,40 +116,10 @@ def create_page_with_card_button(self,title: str = 'Climate Analytics',
                 ]
             ),
             html.Br(),
-            # dbc.Row(
-            #         [
-            #             dbc.Col(
-            #                 [
-            #                     dbc.Card(
-            #                         [
-            #                             dbc.CardImg(
-            #                                 style=homepage_icon_style,
-            #                             ),
-            #                             dbc.CardLink(
-            #                                 children=[
-            #                                     dbc.CardImgOverlay(
-            #                                         [
-            #                                             dbc.CardBody(
-            #                                                 html.H3(
-            #                                                     "CTI Analysis",
-            #                                                     style={"margin": "5%"},
-            #                                                 )
-            #                                             )
-            #                                         ]
-            #                                     )
-            #                                 ],
-            #                                 href="cti",
-            #                             ),
-            #                         ],
-            #                         style={"width": "18rem", "height": "18rem"},
-            #                     )
-            #                 ]
-            #             )
-            #         ]
-            #     ),
+
         ],
     )
-    return app_description
+ #   return app_description
 
 
 
