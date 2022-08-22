8f08e16006e8d19c8725a602d716bbab021db5ff
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-22 23:58:12 +0200

    Update models_evaluation.py

--- models/models_evaluation.py
+++ models/models_evaluation.py
@@ -20,6 +20,8 @@ from preprocess_pipeline import (X_train,
                                 y_test
                                 )
 
+from sklearn.metrics import cross_validation
+from sklearn.model_selection import cross_validate, cross_val_score, cross_val_predict
 
 ridge = RidgeCV()
 all_models = [("Radom Forest", rf_pipeline),
@@ -35,3 +37,15 @@ stack_regressors = StackingRegressor(estimators=all_models, final_estimator=ridg
 
 print(knn_rmse_test)
 # %%
+#%%
+for ax, (name, est) in zip(axs, all_models + [('Stacking Regressor', stack_regressors)]):
+  start_time = time.time()
+  score = cross_validate(est, X_train, y_train,
+                         scoring=['r2', 'neg_mean_absolute_error'],
+                         n_jobs=2, verbose=0
+                         )
+  elapsed_time = time.time() - start_time
+  
+  y_pred = cross_val_predict(est, X_train, y_train, 
+                             n_jobs=2, verbose=1)
+  
