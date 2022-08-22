a8b0f35515c9c70ba44dfe9e3d261a71c02a4bdb
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-22 22:30:11 +0200

    Update models_evaluation.py

--- models/models_evaluation.py
+++ models/models_evaluation.py
@@ -1,4 +1,28 @@
 from Co2KnnRegressor import knn_rmse_test
+# make ridgecv model and stack all models together
+from sklearn.linear_model import RidgeCV
+from sklearn.ensemble import  StackingRegressor
+from sklearn.model_selection import train_test_split
+from Co2Rfregressor import rf_pipeline
+from Co2LassoRegressor import lasso_pipeline
+from Co2KnnRegressor import knn_pipeline
+from Co2RidgeRegressor import rd_pipeline
+
+from Co2HistgradientImputed import hgb_pipeline
+from sklearn.metrics import  mean_squared_error, r2_score
+from preprocess_pipeline import (X_train, 
+                                X_test, 
+                                y_train,
+                                y_test
+                                )
+
+
+ridge = RidgeCV()
+all_models = [("Radom Forest", rf_pipeline),
+              ("Lasso", lasso_pipeline),
+              ("Gradient Boosting", hgb_pipeline)
+              ]
+stack_regressors = StackingRegressor(estimators=all_models, final_estimator=ridge)
 
 
 print(knn_rmse_test)
