eda1972e66cc3af20f81c5d3e72c8e2d5fb244f0
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-22 22:29:54 +0200

    Update Co2RidgeRegressor.py

--- models/Co2RidgeRegressor.py
+++ models/Co2RidgeRegressor.py
@@ -1,20 +1,25 @@
 from sklearn.linear_model import RidgeCV
 from sklearn.pipeline import make_pipeline
-from sklearn.metrics import mean_squared_error
-
-from models.preprocess_pipeline import linear_model_preprocess_pipeline
+from sklearn.metrics import mean_squared_error, r2_score
+from preprocess_pipeline import (linear_model_preprocess_pipeline,
+                                 X_train, 
+                                 X_test, 
+                                 y_train,
+                                 y_test
+                                )
 
 #%% ridged model
 rd = RidgeCV(cv=10)
 rd_pipeline = make_pipeline(linear_model_preprocess_pipeline, rd)
 
 #%%
-if '__name__' == '__main__':
-    rd_pipeline.fit(X=X_raw_train, y=y_raw_train)
+if __name__ == '__main__':
+    rd_pipeline.fit(X=X_train, y=y_train)
 
     #%%
-    y_pred_rd = rd_pipeline.predict(X_raw_test)
-    mean_squared_error(y_true=y_raw_test, y_pred=y_pred_rd, squared=False)
+    y_pred_rd = rd_pipeline.predict(X_test)
+    rd_reg_rmse_test = mean_squared_error(y_true=y_test, y_pred=y_pred_rd, squared=False)
+    print(f'Ridged regression test RMSE: {rd_reg_rmse_test}')
 
 
 
