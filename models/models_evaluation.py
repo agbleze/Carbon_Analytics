8c7a432a86762cc5bb2974ac2c9405253ba85d10
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-24 02:35:50 +0200

    Update models_evaluation.py

--- models/models_evaluation.py
+++ models/models_evaluation.py
@@ -1,6 +1,6 @@
 
 #%%
-from Co2KnnRegressor import knn_rmse_test
+#from Co2KnnRegressor import knn_rmse_test
 # make ridgecv model and stack all models together
 from sklearn.linear_model import RidgeCV
 from sklearn.ensemble import  StackingRegressor
@@ -17,10 +17,13 @@ from sklearn.metrics import  mean_squared_error, r2_score
 from preprocess_pipeline import (X_train, 
                                 X_test, 
                                 y_train,
-                                y_test
+                                y_test,
+                                X, y
                                 )
 
-from sklearn.metrics import cross_validation
+import pandas as pd
+
+#from sklearn.metrics import cross_validation
 from sklearn.model_selection import cross_validate, cross_val_score, cross_val_predict
 
 ridge = RidgeCV()
@@ -28,24 +31,46 @@ all_models = [("Radom Forest", rf_pipeline),
               ("Lasso", lasso_pipeline),
               ("Hist Gradient Boosting", hgb_pipeline),
               ("Extreme Gradient Boosting Random Forest", xgb_pipeline),
-              ("KNN", knn_pipeline),
-              ("Ridge", rd_pipeline),
+              ("KNN k=5", knn_pipeline),
+              #("Ridge", rd_pipeline),
               ("SVR rbf", svr_rbf_pipeline)
               ]
 stack_regressors = StackingRegressor(estimators=all_models, final_estimator=ridge)
 
+candidate_models = all_models.copy()
 
-print(knn_rmse_test)
+candidate_models.extend([("Ridge", rd_pipeline), ('stacked_models', stack_regressors)])
+import math 
+if __name__ == '__main__':
+    test_score_dict = []
+    for model_name, mod_pipeline in all_models:              
+        score = cross_validate(estimator=mod_pipeline,
+                               X=X, y=y, cv=10,
+                        scoring=('neg_mean_squared_error'),
+                        return_train_score=False)
+        test_score_dict = {'model': model_name, 'test_score': -(score['test_score'])}
+        # test_score_dict['model'] = model_name
+        # test_score_dict['test_score'] =  -(score['test_score'])
+        #test_score_dict.append({'model': 'KNN', 'test_score': -(score['test_score'])})
+    df = pd.DataFrame(data=test_score_dict)
+    #df['test_RMSE'] = df['test_score'].apply(lambda x: math.sqrt(x))
+    #df.drop(columns='test_score', inplace=True)
+    print(df)
+    #    print(model_name, mod_pipeline)
+    #print(candidate_models)
+        
+        
+
 # %%
 #%%
-for ax, (name, est) in zip(axs, all_models + [('Stacking Regressor', stack_regressors)]):
-  start_time = time.time()
-  score = cross_validate(est, X_train, y_train,
-                         scoring=['r2', 'neg_mean_absolute_error'],
-                         n_jobs=2, verbose=0
-                         )
-  elapsed_time = time.time() - start_time
+# for ax, (name, est) in zip(axs, all_models + [('Stacking Regressor', stack_regressors)]):
+#   start_time = time.time()
+#   score = cross_validate(est, X_train, y_train,
+#                          scoring=['r2', 'neg_mean_absolute_error'],
+#                          n_jobs=2, verbose=0
+#                          )
+#   elapsed_time = time.time() - start_time
   
-  y_pred = cross_val_predict(est, X_train, y_train, 
-                             n_jobs=2, verbose=1)
+#   y_pred = cross_val_predict(est, X_train, y_train, 
+#                              n_jobs=2, verbose=1)
   
