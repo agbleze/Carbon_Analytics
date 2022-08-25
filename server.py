8a82852f5f39d784b60c10fd8e5e05a6efc1765a
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-26 01:58:42 +0200

    Update server.py

--- server.py
+++ server.py
@@ -253,9 +253,9 @@ def render_sector_layout(sector_selected, sector_sidebar_button):
 
 
 
-@callback(Output(component_id='', component_property=''),
-          Output(component_id='', component_property=''),
-          Input(component_id='', component_property='')
+@callback(Output(component_id='id_graph_avg_cv_rmse_plot', component_property='figure'),
+          Output(component_id='id_graph_models_rmse_plot', component_property='figure'),
+          Input(component_id='eval_model_sidebutton', component_property='n_clicks_timestamp')
           )
 def plot_models_error_estimates():
     pass
