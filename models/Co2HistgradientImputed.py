902eee0834de997baeb479e49c98cefbf28ea075
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-25 05:53:24 +0200

    Update Co2HistgradientImputed.py

--- models/Co2HistgradientImputed.py
+++ models/Co2HistgradientImputed.py
@@ -1,9 +1,9 @@
 #%%
 from sklearn.ensemble import HistGradientBoostingRegressor
-from preprocess_pipeline import decision_tree_data_preprocess
+from models.preprocess_pipeline import decision_tree_data_preprocess
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import mean_squared_error
-from preprocess_pipeline import (X_train, 
+from models.preprocess_pipeline import (X_train, 
                                  X_test, 
                                  y_train,
                                  y_test
@@ -14,7 +14,7 @@ hgb = HistGradientBoostingRegressor(random_state=0)
 hgb_pipeline = make_pipeline(decision_tree_data_preprocess, hgb)
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
         #%% histgradientboosting
     hgb_pipeline.fit(X_train, y_train)
     y_pred_hgb = hgb_pipeline.predict(X_test)
