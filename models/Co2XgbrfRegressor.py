c4153409b6e19af947b77f9ba7dc0a0b221e0279
Agbleze <lagbleze@st.ug.edu.gh>
2022-08-26 14:32:42 +0200

    Update Co2XgbrfRegressor.py

--- models/Co2XgbrfRegressor.py
+++ models/Co2XgbrfRegressor.py
@@ -2,12 +2,20 @@ from xgboost import XGBRFRegressor
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import cross_validate, cross_val_score, cross_val_predict
+import joblib 
+# from models.preprocess_pipeline import (decision_tree_data_preprocess, 
+#                                         X_train, 
+#                                         X_test, 
+#                                         y_train,
+#                                         y_test
+#                                         )
 
-from models.preprocess_pipeline import (decision_tree_data_preprocess, 
+
+from preprocess_pipeline import (decision_tree_data_preprocess, 
                                         X_train, 
                                         X_test, 
                                         y_train,
-                                        y_test
+                                        y_test, X, y
                                         )
 
 xgb = XGBRFRegressor()
@@ -21,4 +29,9 @@ if __name__ == '__main__':
     y_pred_xgb = xgb_pipeline.predict(X_test)
     rmse = mean_squared_error(y_true=y_test, y_pred=y_pred_xgb, squared=False)
     print(f'Xggbrf test rmse: {rmse}')
+
+
+    xgb_pipeline.export('xgb_model.py')
+    
+    
 
